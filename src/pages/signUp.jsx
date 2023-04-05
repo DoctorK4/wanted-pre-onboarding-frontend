96f40844f0b095a12f60f98a40b024be079af254
doctork4 <doctor.k1994@gmail.com>
2023-04-05 11:44:32 +0900

    [feature] 회원가입 버튼 클릭 시 API 통신연결 확인 #7

--- src/pages/signUp.jsx
+++ src/pages/signUp.jsx
@@ -1,10 +1,23 @@
+import axios from "axios";
 import { useState } from "react";
+
+const host = 'https://www.pre-onboarding-selection-task.shop/';
+const api = axios.create({
+  baseURL: host,
+  Headers: {
+    'Content-Type' : 'application/json',
+  },
+})
 
 function SignUp (){
   const [ validEmail, setValidEmail ] = useState(false);
   const [ validPassword, setValidPassword ] = useState(false);
+  const [ email, setEmail ] = useState('');
+  const [ password, setPassword ] = useState('');
 
   const validatePassword = (e) => {
+    setPassword(e.target.value);
+
     if (e.target.value.length >= 8){
       setValidPassword(true)
     }
@@ -14,6 +27,8 @@ function SignUp (){
   }
 
   const validateEmail = (e) => {
+    setEmail(e.target.value);
+    
     if (e.target.value.includes('@')){
       setValidEmail(true)
     }
@@ -21,14 +36,26 @@ function SignUp (){
       setValidEmail(false)
     }
   }
- 
+  
+  const register = async () => {
+
+    await api.post('/auth/signup',{
+      email, 
+      password,
+    })
+    .then((res) => {
+      console.log(res);
+    })
+    .catch((err) => console.log(err));
+  }
+
   return (
     <>
       <label htmlFor="email">이메일</label>
-      <input id="email" data-testid="email-input" placeholder="이메일을 입력해주세요" onChange={validateEmail}/>
+      <input id="email" data-testid="email-input" placeholder="이메일을 입력해주세요" type="email" value={email} onChange={validateEmail}/>
       <label htmlFor="password">비밀번호</label>
-      <input id="password" data-testid="password-input" placeholder="비밀번호를 입력해주세요" onChange={validatePassword}/>
-      <button data-testid="signup-button" disabled={!(validEmail && validPassword)}>회원가입</button>
+      <input id="password" data-testid="password-input" placeholder="비밀번호를 입력해주세요" type="password" value={password} onChange={validatePassword}/>
+      <button type="submit" data-testid="signup-button" disabled={!(validEmail && validPassword)} onClick={register}>회원가입</button>
     </>
   ) 
 }
