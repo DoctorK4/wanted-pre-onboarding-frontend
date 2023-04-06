b403115d778869f622de3a02a7d019c31cf954b1
doctork4 <doctor.k1994@gmail.com>
2023-04-06 09:39:03 +0900

    [settings] react-router-dom 설정 및 페이지간이동 구현

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,18 @@
 import SignIn from './pages/signIn';
 import SignUp from './pages/signUp';
+import ToDo from './pages/toDo';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      {/* <SignIn/> */}
-      <SignUp/>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/signup" element={<SignUp />}></Route>
+          <Route path="/signin" element={<SignIn />}></Route>
+          <Route path="/todo" element={<ToDo />}></Route>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
