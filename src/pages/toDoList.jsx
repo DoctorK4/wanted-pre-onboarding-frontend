0e367af2bf072f8d299841a5e8b7fd92ac0f8ec0
doctork4 <doctor.k1994@gmail.com>
2023-04-14 23:03:10 +0900

    [enhancement] todolist 로컬 구현 & todolist 비었을 시 문구구현

--- src/pages/toDoList.jsx
+++ src/pages/toDoList.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import ToDo from '../components/ToDo';
 import axios from "axios";
 
@@ -16,18 +16,39 @@ function ToDoList () {
   const [inputValue, setInputValue] = useState('');
   const [toDos, setToDos] = useState([]);
 
+  // useEffect(()=>{
+  //   getToDos();
+  // },[])
+
+  // // DB에서 할일 목록을 가져오는 함수
+  // const getToDos = async () => {
+  //   let newdata = await api.get('/todos').then((res)=>res.data);
+  //   setToDos([...toDos, newdata]);
+    // .then((res)=> res.data.map(
+    //   (item, index) => 
+    //   <ToDo 
+    //     key={index} 
+    //     index={index} 
+    //     inputValue={item.todo} 
+    //     toDos={toDos} 
+    //     setToDos={setToDos} 
+    //     isCompleted={item.isCompleted}
+    //   />)
+ 
+
   const addToDo = async (e) => {
     e.preventDefault();
-    // 입력창 초기화
+    // DB의 할일 목록 리스트에 새 할일 추가
     await api.post('/todos',{
       "id": toDos.findIndex((item)=>item===inputValue),
       "todo": inputValue,
       "isCompleted": false,
-      "userId": token,
     })
     .then((res) => {
-      console.log(res);
+      
       setToDos([...toDos, inputValue]);
+
+      // 입력창 초기화
       setInputValue('');
     })
     .catch((err)=>console.log(err));
@@ -36,11 +57,35 @@ function ToDoList () {
   return (
     <>
       <form onSubmit={addToDo}>
-        <input data-testid="new-todo-input" type="text" placeholder="할일을 입력하세요" value={inputValue} onChange={(e)=>setInputValue(e.target.value)}/>
-        <button data-testid="new-todo-add-button" type="submit">추가</button>
+        <input 
+          data-testid="new-todo-input" 
+          type="text" 
+          placeholder="할일을 입력하세요" 
+          value={inputValue} 
+          onChange={(e) => setInputValue(e.target.value)}
+          />
+        <button 
+          data-testid="new-todo-add-button" 
+          type="submit"
+          disabled={(inputValue) ? false : true}
+        >
+          추가
+        </button>
       </form>
       <ul>
-        {toDos.map((inputValue, index)=>(<ToDo key={index} index={index} inputValue={inputValue} toDos={toDos} setToDos={setToDos}/>))}
+        {toDos.length === 0 ? 
+          <li>to-do-list is empty</li> 
+        : 
+          toDos.map((inputValue, index) => 
+            (<ToDo 
+              key={index} 
+              index={index} 
+              inputValue={inputValue} 
+              toDos={toDos} 
+              setToDos={setToDos} 
+            />)
+          )
+        }
       </ul>
     </>
   )
